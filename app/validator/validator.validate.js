c055585cb193ab4c6cf17919a5b728fb6025729a
roboticsza <robotics.za@gmail.com>
2021-02-11 06:55:32 +0700

    add backet

--- app/validator/validator.validate.js
+++ app/validator/validator.validate.js
@@ -1,14 +1,16 @@
 const Joi = require('@hapi/joi')
 
-const validate  = (schema, payload) => {
-  const schemaObject = Joi.object(schema)
-  const {error, _} = schemaObject.validate(payload)
-  if (error) {
-    if (process.env.NODE_ENV === 'development')
-      console.warn('[error message] ', error.details[0].message)
-    return false
+const validate  = (schema) => {
+  return (req, res, next) => {
+    const schemaObject = Joi.object(schema)
+    const {error, _} = schemaObject.validate(req.body)
+    if (error) {
+      if (process.env.NODE_ENV === 'development')
+        console.warn('[error message] ', error.details[0].message)
+      return res.status(400).send('Bad request.')
+    }
+    next()
   }
-  return true
 }
 
 module.exports = { validate }
