ad5a6bd9436259dd594dd259fcdb8dc9a688b91d
roboticsza <robotics.za@gmail.com>
2021-02-11 06:57:35 +0700

    add auth to check from firebase

--- config/express.js
+++ config/express.js
@@ -4,9 +4,15 @@ const logger = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors')
 
+// firebase admin
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json")
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
 const app = express()
 
-app.use(cors)
 app.use(logger('dev'))
 app.use(cookieParser())
 app.use(helmet())
@@ -14,20 +20,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-let authorized = true
-function checkAuth(req, res, next) {
-  if (authorized) {
-    next()
-  } else {
-    res.status(403).send('Unauthorized!')
-    return
-  }
-}
-
 // router 
-app.use('/', checkAuth)
+app.use('/', require('../app/routes/route.auth')(admin))
 app.use('/signup', require('../app/routes/route.signup'))
 app.use('/on-trending', require('../app/routes/route.on_trending'))
 // app.use('/user', require('../app/routes/route.user'))
-
 module.exports = app
