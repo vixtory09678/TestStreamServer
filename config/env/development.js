989887692aa039ed3456eb5a3e042d79b22e2d97
roboticsza <robotics.za@gmail.com>
2021-02-11 06:56:59 +0700

    config env

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 const config = {
-  ffmp_path = 'C:/Program Files/ffmpeg/bin/ffmpeg.exe',
-  API_PORT = 3000
+  ffmp_path: 'C:/Program Files/ffmpeg/bin/ffmpeg.exe',
+  API_PORT: 3000
 }
 
 module.exports = config
--- config/env/index.js
+++ config/env/index.js
@@ -1,3 +1,7 @@
-const config = process.env.NODE_ENV === 'development' ? require('./development') : require('./production')
-
+let config = {}
+if (process.env.NODE_ENV !== 'development'){
+  config = require('./development')
+} else {
+  config = require('./production')
+}
 module.exports = config
