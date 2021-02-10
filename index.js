3bb9ad75eb6832cb4322bd5e125b480df405a0cd
roboticsza <robotics.za@gmail.com>
2021-02-11 05:22:50 +0700

    run both server streamming and restful api

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-const NodeMediaServer = require('node-media-server');
+const streamming = require('./config/streamming')
+const API = require('./config/express')
+const env_config = require('./config/env')
 
-const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60
-  },
-  http: {
-    port: 8000,
-    mediaroot: './media',
-    allow_origin: '*'
-  },
-  trans: {
-    ffmpeg: '/home/ubuntu/ffmpeg-4.3.1/ffmpeg',
-    tasks: [
-      {
-        app: 'live',
-        hls: true,
-        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-        dash: true,
-        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-      }
-    ]
-  }
-};
 
-var nms = new NodeMediaServer(config)
-nms.run();
+streamming.run()
+
+API.listen(env_config.API_PORT, () => {
+  console.log(`Server run at port ${env_config.API_PORT}`)
+})
